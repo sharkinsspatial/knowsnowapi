f29a633a97f845685481a1bc32ad668fa5ede5c0
sharkinsspatial <sharkinsgis@gmail.com>
2015-11-05 12:16:03 -0300

    Modified responses to include query parameters after React Router hash in url.

--- server/boot/facebook-oauth.js
+++ server/boot/facebook-oauth.js
@@ -8,15 +8,9 @@ module.exports = function(app) {
     var strategy = 'facebook-login';
     var opts = require('../../providers.json')[strategy];
 
-    // Doesn't look like we need this..yet, but it would be good to set proper options
-    // up front so we can use the proper values directly below.
     opts.session = opts.session !== false;
 
     opts.customCallback = function (req, res, next) {
-        // We need url, because we want to use to to parse the url and then reformat it with params.
-        var url = require('url');
-
-        // Note that we have to only use variables that are in scope right now, like opts.
         passport.authenticate(
             strategy,
             {session: false},
@@ -30,19 +24,9 @@ module.exports = function(app) {
                     // TODO - we might want to add some params here too for failures.
                     return res.redirect(opts.failureRedirect);
                 }
-                // Add the tokens to the callback as params.
-                var redirect = url.parse(opts.successRedirect, true);
 
-                // this is needed or query is ignored. See url module docs.
-                delete redirect.search;
-
-                redirect.query = {
-                    'access_token': info.accessToken.id,
-                    // Note the .toString here is necessary.
-                    'userId': user.id.toString()
-                };
-                // Put the url back together. It should now have params set.
-                redirect = url.format(redirect);
+                var redirect = opts.successRedirect + '?' + 'access_token=' +
+                    info.accessToken.id + '&userId=' + user.id.toString()
                 return res.redirect(redirect);
             }
         )(req, res, next);
