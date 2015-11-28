775abed71179b09d90eb165b0809b644fac041f7
sharkinsspatial <sharkinsgis@gmail.com>
2015-11-28 12:01:25 -0300

    Updated verifyHref to support external Heroku hostnames.

--- server/boot/userEmailVerification.js
+++ server/boot/userEmailVerification.js
@@ -4,14 +4,24 @@ module.exports = function(app) {
     var User = app.models.User;
     User.afterRemote('create', function(context, user, next) {
         var redirect = encodeURIComponent(process.env.SUCCESS_REDIRECT);
+        var userModel = user.constructor;
+        var verifyHref = 'http://'
+            + process.env.HOST
+            + '/api'
+            + userModel.http.path
+            + '/confirm'
+            + '?uid='
+            + user.id
+            + '&redirect='
+            + redirect
+
         var options = {
             type: 'email',
             to: user.email,
             from: 'noreply@knowsnow.ca',
             subject: 'Thanks for registering for Know Snow',
             template: path.resolve(__dirname, '../../node_modules/loopback/templates/verify.ejs'),
-            redirect: redirect,
-            host: process.env.HOST
+            verifyHref: verifyHref
         };
 
         user.verify(options, function(err, response, next) {
