c769959fb6a02250d3b739158d69354617ea3fbd
sharkinsspatial <sharkinsgis@gmail.com>
2015-12-04 10:51:16 -0300

    Async refactoring for cloudStoreImage.

--- common/models/cloudStoreImage.js
+++ common/models/cloudStoreImage.js
@@ -22,69 +22,39 @@ module.exports = function (CloudStoreImage) {
         AWS.config.region = app.get('cloudStoreImages').s3.region;
     });
 
-    CloudStoreImage.beforeRemote('upload', function (ctx, unused, next) {
-        /* Sample getting header
-           app.logger.info("I GOT HEADER: " + ctx.req.headers['image-type']);
-           if (!ctx.req.headers['image-type']) {
-           return next('Header \'image-type\' is required.');
-           }
-           */
-
-        next();
-    });
-
-    CloudStoreImage.afterRemote('upload', function (ctx, res, next) {
-        //var header = ctx.req.headers['image-type'];
-        var file = res.result.files.fileUpload[0];
-
-        var fileRoot = CloudStoreImage.app.datasources.fileStorageDS.settings.root;
-
-        var ext = getExtension(file.name);
-        var fileNameRoot = file.name.substr(0, file.name.length - ext.length);
-
-        var filePath = fileRoot + '/' + file.container + '/' + file.name;
+    function buildImageMetadata(filePath, file, callback) {
         fs.open(filePath, 'r', function(status, fd) {
-            if (status) {
-                console.log(status);
-                return;
-            }
             var buffer = new Buffer(65635);
             fs.read(fd, buffer, 0, 65635, 0, function(err, bytesRead, buffer) {
+                if (err) return callback(err);
+
                 var parser = exif.create(buffer);
                 var result = parser.parse();
-                CloudStoreImage.app.models.Report.findById(file.container, function(err, instance) {
-                    if (err) {
-                        console.log(err);
-                        next();
-                    }
-                    var imageMetadata = { "name": file.name }
-                    if (result.tags.GPSLongitude) {
-                        imageMetadata.latitude = result.tags.GPSLatitude;
-                        imageMetadata.longitude = result.tags.GPSLongitude;
-                    }
-                    instance.imageMetadatas.create(imageMetadata,
-                        function(err, result) {
-                            var test = result;
-                        });
-                });
 
+                CloudStoreImage.app.models.Report.findById(file.container,
+                    function(err, instance) {
+                        if (err) return callback(err);
+
+                        var imageMetadata = { "name": file.name }
+                        if (result.tags.GPSLongitude) {
+                            imageMetadata.latitude = result.tags.GPSLatitude;
+                            imageMetadata.longitude = result.tags.GPSLongitude;
+                        }
+                        instance.imageMetadatas.create(imageMetadata,
+                            function(err, result) {
+                                if (err) return callback(err);
+                                callback(null, result);
+                            });
+                    });
             });
-
         });
+    }
+
+    function buildResizedImages(filePath, fileRoot, file, resize, callback) {
+        var ext = getExtension(file.name);
+        var fileNameRoot = file.name.substr(0, file.name.length - ext.length);
         var filePathRoot = fileRoot + '/' + file.container + '/' + fileNameRoot;
-
-        var resize = [
-            {width: app.get('cloudStoreImages').thumbWidth, ext: app.get('cloudStoreImages').thumbExt},
-            {width: app.get('cloudStoreImages').displayWidth, ext: app.get('cloudStoreImages').displayExt},
-        ];
-
-        var s3obj;
-
-        async.eachSeries(resize, function (spec, cb) {
-            //if (spec.width === 0) {
-                //return cb();
-            //}
-
+        async.eachSeries(resize, function (spec, callback) {
             spec.filePath = filePathRoot + spec.ext;
             spec.fileName = fileNameRoot + spec.ext;
 
@@ -92,75 +62,101 @@ module.exports = function (CloudStoreImage) {
                 src: filePath,
                 dst: spec.filePath,
                 width: spec.width
-                }, function (err, path) {
-                    if (err) {
-                        return cb(err);
-                    }
-
-                    cb();
+            }, function (err, path) {
+                if (err) return callback(err);
+                callback();
             });
         },
-        function (err) {
-            if (err) {
-                return next(err);
-            }
+        function(err) {
+            if (err) return callback(err);
+            callback();
+        });
+    }
 
-            async.eachSeries(resize, function (spec, cb) {
-                fs.readFile(spec.filePath, function (err, buffer) {
+    function uploadS3(resize, file, callback) {
+        var s3obj;
+        async.eachSeries(resize, function (spec, callback) {
+            fs.readFile(spec.filePath, function (err, buffer) {
+                if (err) return callback(err);
 
-                    s3obj = new AWS.S3(
-                        {
-                        params: {
-                            Bucket: app.get('cloudStoreImages').s3.bucket,
-                            Key: file.container + '/' + spec.fileName,
-                            ContentType: app.get('cloudStoreImages').contentType,
-                            ACL: 'public-read'
-                        }
-                    });
-
-                    s3obj.upload({Body: buffer})
-                    .on('httpUploadProgress', function (evt) {
-                        //app.logger.debug(evt);
-                    }).send(function (err, data) {
-                        if (err) {
-                            return cb(err);
-                        }
-
-                        spec.aws = data;
-
-                        cb();
-                    });
-                });
-            },
-            function (err) {
-                if (err) {
-                    //app.logger.error(err);
-                    return next(err);
-                }
-
-                // Return the files we've uploaded
-                res.result.files.fileUpload[0].path = filePath;
-                res.result.files.fileUpload[0].resize = resize;
-
-                //Return success to the client.  No need to wait for the files to be deleted.
-                next();
-
-                // Delete the local storage
-                async.eachSeries(resize, function (spec, cb) {
-                    fs.unlink(spec.filePath, function (err) {
-                        if (err) {
-                            return cb(err);
-                        }
-
-                        cb();
-                    })
-                },
-                function (err) {
-                    if (err) {
-                        console.log(err);
+                s3obj = new AWS.S3(
+                    {
+                    params: {
+                        Bucket: app.get('cloudStoreImages').s3.bucket,
+                        Key: file.container + '/' + spec.fileName,
+                        ContentType: app.get('cloudStoreImages').contentType,
+                        ACL: 'public-read'
                     }
                 });
+
+                s3obj.upload({Body: buffer}).on('httpUploadProgress',
+                    function (evt) {}).send(function (err, data) {
+                        if (err) return callback(err);
+
+                        spec.aws = data;
+                        callback();
+                    });
             });
+        },
+        function(err) {
+            if (err) return callback(err);
+            callback();
+        });
+    }
+
+    function deleteImages(resize, filePath, callback) {
+        async.eachSeries(resize, function (spec, callback) {
+            fs.unlink(spec.filePath, function (err) {
+                if (err) return callback(err);
+                callback();
+            });
+        },
+        function(err) {
+            if (err) return callback(err);
+            fs.unlink(filePath, function (err) {
+                if (err) return callback(err);
+                callback();
+            });
+        });
+    }
+
+    CloudStoreImage.afterRemote('upload', function (ctx, res, next) {
+        var file = res.result.files.fileUpload[0];
+        var fileRoot = CloudStoreImage.app.datasources.fileStorageDS.settings.root;
+        var filePath = fileRoot + '/' + file.container + '/' + file.name;
+        var resize = [
+            {width: app.get('cloudStoreImages').thumbWidth,
+                ext: app.get('cloudStoreImages').thumbExt},
+            {width: app.get('cloudStoreImages').displayWidth,
+                ext: app.get('cloudStoreImages').displayExt}
+        ];
+
+        var locals = {};
+
+        async.parallel([
+            function(callback) {
+                buildImageMetadata(filePath, file, function(err, result) {
+                    if (err) return callback(err);
+                    locals = result;
+                    callback();
+                });
+            },
+            function(callback) {
+                async.series([
+                    async.apply(buildResizedImages, filePath, fileRoot, file, resize),
+                    async.apply(uploadS3, resize, file),
+                    async.apply(deleteImages, resize, filePath)
+                ],
+                function(err) {
+                    if (err) return callback(err);
+                    callback();
+                });
+            }
+        ],
+        function(err) {
+            if (err) return next(err);
+            res.imageMetadata = locals;
+            next();
         });
     });
 
